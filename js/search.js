b1f7ed5c4fe4b1ea75d7c470496c1746cbabf826
Varun Bansal <varun13168@iiitd.ac.in>
2015-10-23 18:10:07 +0530

    category page added

---
+++ js/search.js
@@ -0,0 +1,46 @@
+// $("#search").click(function() {
+//     $(this).css("background-color","#232C3C");
+//     $(this).css("color","white");
+//     $("#searchWrap").animate({width:'96%'},200).css({'color':'#white'});
+//     $('#searchWrap').show(1000); 
+// });
+
+  $(document).ready(function(){
+            var submitIcon = $('.searchbox-icon');
+            var inputBox = $('.searchbox-input');
+            var searchBox = $('.searchbox');
+            var isOpen = false;
+            submitIcon.click(function(){
+                if(isOpen == false){
+                    searchBox.addClass('searchbox-open');
+                    inputBox.focus();
+                    isOpen = true;
+                } else {
+                    searchBox.removeClass('searchbox-open');
+                    inputBox.focusout();
+                    isOpen = false;
+                }
+            });  
+             submitIcon.mouseup(function(){
+                    return false;
+                });
+            searchBox.mouseup(function(){
+                    return false;
+                });
+            $(document).mouseup(function(){
+                    if(isOpen == true){
+                        $('.searchbox-icon').css('display','block');
+                        submitIcon.click();
+                    }
+                });
+        });
+            function buttonUp(){
+                var inputVal = $('.searchbox-input').val();
+                inputVal = $.trim(inputVal).length;
+                if( inputVal !== 0){
+                    $('.searchbox-icon').css('display','none');
+                } else {
+                    $('.searchbox-input').val('');
+                    $('.searchbox-icon').css('display','block');
+                }
+            }
