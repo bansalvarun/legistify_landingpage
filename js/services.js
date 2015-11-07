17d350d1cb0b5b17e829dc33eab57775ba8a0dba
bansalvarun <varun13168@iiitd.ac.in>
2015-11-07 20:27:22 +0530

    bugs fixed

--- js/services.js
+++ js/services.js
@@ -30,31 +30,31 @@ $(document).ready(function(){
 		$('.floatCenterOnMobile').attr("style","float:center");
 	
 	$( "#findLawyersBlock" ).hover(function() {
-	  $( "#findLawyers" ).attr( "style","display:block" );
-	  $( "#ourServices" ).attr( "style","display:none" );
+	  $( "#findLawyers" ).attr( "style","display:block;max-height:200px;overflow:hidden;" );
+	  $( "#ourServices" ).attr( "style","display:none;" );
 	  $( "#findLawyersBlock2" ).css({ "display": "block", "opacity": "0" }).animate({ "opacity": "1" }, 1000);
 	  $( "#findLawyersBlock1" ).attr( "style","display:none" );
 	},function(){
 	  $( "#findLawyers" ).attr( "style","display:none" );
-	  $( "#ourServices" ).attr( "style","display:block" );
+	  $( "#ourServices" ).attr( "style","display:block;max-height:200px;overflow:hidden;" );
 	  $( "#findLawyersBlock1" ).css({ "display": "block", "opacity": "0" }).animate({ "opacity": "1" }, 1000);
 	  $( "#findLawyersBlock2" ).attr( "style","display:none" );
 	});
 
 
 	$( "#makeDocumentsBlock" ).hover(function() {
-	  $( "#makeDocuments" ).attr( "style","display:block" );
+	  $( "#makeDocuments" ).attr( "style","display:block;max-height:200px;overflow:hidden;" );
 	  $( "#ourServices" ).attr( "style","display:none" );
 	  $( "#makeDocumentsBlock2" ).css({ "display": "block", "opacity": "0" }).animate({ "opacity": "1" }, 1000);
 	  $( "#makeDocumentsBlock1" ).attr( "style","display:none" );
 	 },function() {
 	  $( "#makeDocuments" ).attr( "style","display:none" );
-	  $( "#ourServices" ).attr( "style","display:block" );
+	  $( "#ourServices" ).attr( "style","display:block;max-height:200px;overflow:hidden;" );
 	  $( "#makeDocumentsBlock1").css({ "display": "block", "opacity": "0" }).animate({ "opacity": "1" }, 1000);
 	  $( "#makeDocumentsBlock2" ).attr( "style","display:none" );
 	});	
 	$( "#startResearchBlock" ).hover(function() {
-	  $( "#startResearch" ).attr( "style","display:block" );
+	  $( "#startResearch" ).attr( "style","display:block;max-height:200px;overflow:hidden;" );
 	  $( "#ourServices" ).attr( "style","display:none" );
 	  $( "#startResearchBlock2" ).css({ "display": "block", "opacity": "0" }).animate({ "opacity": "1" }, 2000);
 	  // $( "#startResearchBlock2" ).css({ "display": "block", "width": "0" }).animate({ "width": "100%" }, 1000);
